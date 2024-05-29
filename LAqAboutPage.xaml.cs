42d81d2f4d40578ace38a2530bc18ab205932853
Ripleonardo000 <leonardoandrade2003.197@gmail.com>
2024-05-29 11:27:12 -0500

    Modificar el Shell de la aplicación Mover las imágenes con Visual Studio Control del evento Clicked Ejecución de la aplicación

---
+++ LAqAboutPage.xaml.cs
@@ -0,0 +1,15 @@
+namespace Leonardo_Andrade_PROYECTOAPUNTES;
+
+public partial class LAqAboutPage : ContentPage
+{
+	public LAqAboutPage()
+	{
+		InitializeComponent();
+	}
+
+    private async void LearnMore_Clicked(object sender, EventArgs e)
+    {
+        // Navigate to the specified URL in the system browser.
+        await Launcher.Default.OpenAsync("https://aka.ms/maui");
+    }
+}
